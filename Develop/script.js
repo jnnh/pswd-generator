9f6a2d135baadea2ab347427813a18f15321d840
jnnh <jenniferhorvath@cmail.carleton.ca>
2021-12-12 13:54:50 -0500

    deleted most code

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,3 @@
-// Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 
 var passwordCriteria = function(){
   debugger;
@@ -30,30 +12,7 @@ var passwordCriteria = function(){
     }
 };
 
-// Function to Generate Arrays using ASCII Character Table
-var arrayFromLowToHigh = function(low,high){
-  var array = []
-  for (var i=low; i <= high; i++) {
-    array.push(i)
-  }
-  return array
-}
-
-// Variable Arrays
-var lowerCaseOptions = arrayFromLowToHigh(97,122);
-var upperCaseOptions = arrayFromLowToHigh(65,90);
-var numberOptions = arrayFromLowToHigh(48,57);
-var specialCharacterOptions = arrayFromLowToHigh(33,47).concat(arrayFromLowToHigh(58,64)).concat(arrayFromLowToHigh(91,96)).concat(arrayFromLowToHigh(123,126));
-
-//Function to 
-
-
-//Create a Concated Array for Password based on Password Criteria
-var passwordArray = function (lowerCaseOptions, upperCaseOptions, numberOptions, specialCharacterOptions){
-  confirmCriteria ()
-}
-
 passwordCriteria()
 
 console.log(promptLength);
-//console.log(confirmLowerCase);
+
