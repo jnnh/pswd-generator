ac82a75f69c4ad691230afaf29e2e13c4cb661d8
jnnh <jenniferhorvath@cmail.carleton.ca>
2021-12-12 12:21:57 -0500

    generated variable arrays

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,34 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+/*var passwordCriteria = function(){
+  var promptLength = prompt("How many characters would you like in your password?");
+    if (promptLength < 8 || promptLength > 128 || !promptLength){
+      alert("Password Length must be between 8 and 128 characters. Please try again.");
+      passwordCriteria ();
+    }
+    else {
+      return promptLength;
+    }
+  var confirmLowerCase = confirm("Would you like lowercase characters?")
+}*/
+
+
+var arrayFromLowToHigh = function(low,high){
+  var array = []
+  for (var i=low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+var lowerCaseOptions = arrayFromLowToHigh(97,122);
+console.log(lowerCaseOptions);
+var upperCaseOptions = arrayFromLowToHigh(65,90);
+console.log(upperCaseOptions);
+var numberOptions = arrayFromLowToHigh(48,57);
+console.log(numberOptions);
+var specialCharacterOptions = arrayFromLowToHigh(33,47).concat(arrayFromLowToHigh(58,64)).concat(arrayFromLowToHigh(91,96)).concat(arrayFromLowToHigh(123,126));
+console.log(specialCharacterOptions);
