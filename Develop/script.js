0d06ae18ac3364404a8d4ec616cb91245138bfd6
jnnh <jenniferhorvath@cmail.carleton.ca>
2021-12-12 22:49:59 -0500

    fix prompt flow

--- Develop/script.js
+++ Develop/script.js
@@ -2,78 +2,79 @@
 var generateBtn = document.querySelector("#generate");
 
 // Function to Generate Arrays using ASCII Character Table
-var arrayFromLowToHigh = function(low,high){
+var arrayFromLowToHigh = function (low, high) {
   var array = []
-  for (var i=low; i <= high; i++) {
+  for (var i = low; i <= high; i++) {
     array.push(i);
   }
   return array;
 };
 
 // Arrays of ASCII Characters
-var lowerCaseOptions = arrayFromLowToHigh(97,122);
-var upperCaseOptions = arrayFromLowToHigh(65,90);
-var numberOptions = arrayFromLowToHigh(48,57);
-var specialOptions = arrayFromLowToHigh(33,47).concat(arrayFromLowToHigh(58,64)).concat(arrayFromLowToHigh(91,96)).concat(arrayFromLowToHigh(123,126));
+var lowerCaseOptions = arrayFromLowToHigh(97, 122);
+var upperCaseOptions = arrayFromLowToHigh(65, 90);
+var numberOptions = arrayFromLowToHigh(48, 57);
+var specialOptions = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126));
 
 //confirm password criteria
-var passwordCriteria = function(){
+var passwordCriteria = function () {
   var promptLength = prompt("How many characters would you like in your password?");
-  promptLength= parseInt(promptLength);
-    if (promptLength < 8 || promptLength > 128 || !promptLength){
-      alert("Password length must be a number between 8 and 128. Please try again.");
+    promptLength = parseInt(promptLength);
+  while (promptLength < 8 || promptLength > 128 || !promptLength) {
+    alert("Password length must be a number between 8 and 128. Please try again.");
+    promptLength = prompt("How many characters would you like in your password?");
+    promptLength = parseInt(promptLength);
+  }
+    var confirmLowerCase = confirm("Would you like to include lower case characters?");
+    var confirmUpperCase = confirm("Would you like to include upper case characters?");
+    var confirmNumbers = confirm("Would you like to include numbers?");
+    var confirmSpecial = confirm("Would you like to include special characters?");
+    var pswdObject = {
+      length: promptLength,
+      lowercase: confirmLowerCase,
+      uppercase: confirmUpperCase,
+      numbers: confirmNumbers,
+      special: confirmSpecial
+    }
+    if (!confirmLowerCase && !confirmUpperCase && !confirmNumbers && !confirmSpecial) {
+      alert("you need to choose at least one character type.");
       passwordCriteria();
     }
-  var confirmLowerCase = confirm("Would you like to include lower case characters?");
-  var confirmUpperCase = confirm("Would you like to include upper case characters?");
-  var confirmNumbers = confirm("Would you like to include numbers?");
-  var confirmSpecial = confirm("Would you like to include special characters?");
-  var pswdObject = {
-    length: promptLength,
-    lowercase: confirmLowerCase,
-    uppercase: confirmUpperCase,
-    numbers: confirmNumbers,
-    special: confirmSpecial
-  }
-  if (!confirmLowerCase && !confirmUpperCase && !confirmNumbers && !confirmSpecial){
-    alert("you need to choose at least one character type.");
-    passwordCriteria();
-  }
-  return pswdObject;
+    return pswdObject;
 };
 
 //Create a Concated Array for Password based on Password Criteria
-var passwordOptions = function (options){
+var passwordOptions = function (options) {
   var compiledOptions = [];
-  if (options.lowercase){
+  if (options.lowercase) {
     compiledOptions = compiledOptions.concat(lowerCaseOptions);
   }
-  if(options.uppercase){
+  if (options.uppercase) {
     compiledOptions = compiledOptions.concat(upperCaseOptions);
   }
-  if (options.numbers){
+  if (options.numbers) {
     compiledOptions = compiledOptions.concat(numberOptions);
   }
-  if (options.special){
+  if (options.special) {
     compiledOptions = compiledOptions.concat(specialOptions);
   }
   return compiledOptions;
 };
 
 //To generate password
-var generatePassword = function (){
-  var pswdObject = passwordCriteria ();
-  var options = passwordOptions (pswdObject);
+var generatePassword = function () {
+  var pswdObject = passwordCriteria();
+  var options = passwordOptions(pswdObject);
   var passwordArray = [];
-  for (var i=0; i< pswdObject.length; i++) {
-    var character= options[Math.floor(Math.random() * options.length)];
+  for (var i = 0; i < pswdObject.length; i++) {
+    var character = options[Math.floor(Math.random() * options.length)];
     passwordArray.push(String.fromCharCode(character));
   }
-  var password= passwordArray.join('');
+  var password = passwordArray.join('');
   writePassword(password);
 };
 // Write password to the #password input
-var writePassword = function(password) {
+var writePassword = function (password) {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 };
